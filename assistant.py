98c0d953a6ea4ba740bdba2f44636c93fbc78a79
tauri_s <tauristclaire@gmail.com>
2024-08-21 14:48:03 -0700

    Change all pseudocode comments from todos to explanatory comments

--- assistant.py
+++ assistant.py
@@ -14,23 +14,23 @@ assistant = client.beta.assistants.create(
 #create a thread
 thread = client.beta.threads.create()
 
-#TODO: prompt the user for input
+#prompt the user for input
 user_input = input("You: ")
 
-#TODO: use the prompt to create a message within the thread
+#use the prompt to create a message within the thread
 message = client.beta.threads.messages.create(
     thread_id = thread.id,
     role = "user",
     content = user_input
 )
 
-#TODO: create a run
+#create a run
 run = client.beta.threads.runs.create(
     thread_id = thread.id,
     assistant_id = assistant.id
 )
 
-#TODO: monitor the run status at regular intervals until the status of the run is complete
+#monitor the run status at regular intervals until the status of the run is complete
 while True:
     #add a one-second delary between each run status check
     time.sleep(1)
@@ -42,11 +42,11 @@ while True:
     if run_check.status == "completed":
         break
 
-#TODO: extract the most recent message content when the run is completed
+#extract the most recent message content when the run is completed
 thread_messages = client.beta.threads.messages.list(
     thread_id = thread.id
 )
 message_for_user = thread_messages.data[0].content[0].text.value
 
-#TODO: display the message to the user
+#display the message to the user
 print("Assistant: " + message_for_user)
