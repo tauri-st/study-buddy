bbe1e0dae7c85819e64b3eeeec5ef33a34f77f37
tauri_s <tauristclaire@gmail.com>
2024-09-04 16:07:07 -0700

    Comment in all code to create runs and threads and add code to create citations

--- assistant.py
+++ assistant.py
@@ -43,77 +43,98 @@ assistant = client.beta.assistants.update(
 #exit()
 
 #Return appropriate response based on status value
-#def status_message(run_status):
-    #if run_status == "completed":
+def status_message(run_status):
+    if run_status == "completed":
         #extract the most recent message content when the run is completed
-        #thread_messages = client.beta.threads.messages.list(
-            #thread_id = thread.id
-        #)
-        #message = thread_messages.data[0].content[0].text.value
+        thread_messages = client.beta.threads.messages.list(
+            thread_id = thread.id
+        )
+        message = thread_messages.data[0].content[0].text.value
 
-    #if run.status in ["cancelled", "failed", "expired"]:
-        #message = "An error has occurred, please try again."
+    if run.status in ["cancelled", "failed", "expired"]:
+        message = "An error has occurred, please try again."
 
-    #return message
+    return message
 
 #check status details
-#def process_run(thread_id, assistant_id):
+def process_run(thread_id, assistant_id):
     #create a run
-    #new_run = client.beta.threads.runs.create(
-        #thread_id = thread_id,
-        #assistant_id = assistant_id
-    #)
+    new_run = client.beta.threads.runs.create(
+        thread_id = thread_id,
+        assistant_id = assistant_id
+    )
 
-    #phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
+    phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
 
     #monitor the run status at regular intervals until the status of the run is complete
-    #while True:
-        #add a one-second delary between each run status check
-        #time.sleep(1)
-        #print(random.choice(phrases) + "...")
+    while True:
+        #add a one-second delay between each run status check
+        time.sleep(1)
+        print(random.choice(phrases) + "...")
         #check the status of the run
-        #run_check = client.beta.threads.runs.retrieve(
-            #thread_id = thread_id,
-            #run_id = new_run.id
-        #)
-        #if run_check.status in ["cancelled", "failed", "completed", "expired"]:
-            #return run_check
+        run_check = client.beta.threads.runs.retrieve(
+            thread_id = thread_id,
+            run_id = new_run.id
+        )
+        if run_check.status in ["cancelled", "failed", "completed", "expired"]:
+            return run_check
         
 #log error if status is cancelled, failed, or expired
-#def log_run(run_status):
-    #if run_status in ["cancelled", "failed", "completed", "expired"]:
-        #log.error("\nDate: " + str(datetime.datetime.now()) + "\nRun status: " + str(run_status))
+def log_run(run_status):
+    if run_status in ["cancelled", "failed", "completed", "expired"]:
+        log.error("\nDate: " + str(datetime.datetime.now()) + "\nRun status: " + str(run_status))
 
 #create a thread
-#thread = client.beta.threads.create()
+thread = client.beta.threads.create()
 
-#user_input = ""
+user_input = ""
 
-#while True:
-    #if (user_input == ""):
-        #user_input = input("Hello! Let's chat! You can type `exit` to exit out anytime. What's your name? ")
+while True:
+    if (user_input == ""):
+        user_input = input("Hello! Let's chat! You can type `exit` to exit out anytime. What's your name? ")
         #the model doesn't always recognize the answer as a name
         #hand it directly to the chatbot identified as the user's nameso
-        #user_name = f"User name is {user_input}"
-    #else:
+        user_name = f"User name is {user_input}"
+    else:
         #prompt the user for input
-        #user_input = input("You: ")
+        user_input = input("You: ")
 
-    #if user_input.lower() == "exit":
-        #print("Bye bye now, have a good one!")
-        #exit()
+    if user_input.lower() == "exit":
+        print("Bye bye now, have a good one!")
+        exit()
 
     #use the prompt to create a message within the thread
-    #message = client.beta.threads.messages.create(
-        #thread_id = thread.id,
-        #role = "user",
-        #content = user_input
-    #)
+    message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = user_input
+    )
 
-    #run = process_run(thread.id, assistant.id)
+    run = process_run(thread.id, assistant.id)
 
-    #log_run(run.status)
+    log_run(run.status)
 
-    #message = status_message(run.status)
+    message = status_message(run.status)
 
-    #print("\nAssistant: " + message + "\n")
+    print("\nAssistant: " + message + "\n")
+
+    # Use the create and poll SDK helper to create a run and poll the status of
+    # the run until it's in a terminal state.
+
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id, assistant_id=assistant.id
+    )
+
+    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+    message_content = messages[0].content[0].text
+    annotations = message_content.annotations
+    citations = []
+    for index, annotation in enumerate(annotations):
+        message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+        if file_citation := getattr(annotation, "file_citation", None):
+            cited_file = client.files.retrieve(file_citation.file_id)
+            citations.append(f"[{index}] {cited_file.filename}")
+
+    print(message_content.value)
+    print("\n".join(citations))
