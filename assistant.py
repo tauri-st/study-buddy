f91b69506c8cc3055bb134f5102aef5632c3bd64
tauri_s <tauristclaire@gmail.com>
2024-09-04 15:49:32 -0700

    Uncomment all code to create a run threads and messages

--- assistant.py
+++ assistant.py
@@ -11,23 +11,10 @@ logging.basicConfig(filename='assistant.log', level=logging.INFO)
 
 client = OpenAI()
 
-#upload file for assistants
-#rb stands for "read" "binary"
-#open() returns a file object
-#curriculum_knowledge = client.files.create(
-    #file = open("knowledge/OpenAIChatCompletionsAPICheatsheet.pdf", "rb"),
-    #purpose = "assistants"
-#)
-
-#print(curriculum_knowledge)
-
 #create assistant object, with file ids passed to it
 assistant = client.beta.assistants.retrieve(
     assistant_id = "asst_ndwvfyrmD9ACUGBp4o94D3pw"
 )
-
-#print(assistant)
-#exit()
 
 # Update the assistant with the new file
 # To make the files accessible to your assistant, update the assistant’s tool_resources with the new vector_store id.
@@ -36,6 +23,18 @@ assistant = client.beta.assistants.update(
     tool_resources={"file_search": {"vector_store_ids": ["vs_k2nCdJ1xhYkJKkLAU9H7nRx2"]}},
 )
 
+#print(assistant)
+
+#upload file for assistants
+#rb stands for "read" "binary"
+#open() returns a file object
+#curriculum_knowledge = client.files.create(
+    #file = open("OpenAIChatCompletionsAPICheatsheet.pdf", "rb"),
+    #purpose = "assistants"
+#)
+
+#print(curriculum_knowledge)
+
 #assistant_files = client.beta.assistants.files.list(
     #"asst_ndwvfyrmD9ACUGBp4o94D3pw"
 #)
@@ -43,78 +42,78 @@ assistant = client.beta.assistants.update(
 #print(assistant_files)
 #exit()
 
-# #Return appropriate response based on status value
-# def status_message(run_status):
-#     if run_status == "completed":
-#         #extract the most recent message content when the run is completed
-#         thread_messages = client.beta.threads.messages.list(
-#             thread_id = thread.id
-#         )
-#         message = thread_messages.data[0].content[0].text.value
+#Return appropriate response based on status value
+def status_message(run_status):
+    if run_status == "completed":
+        #extract the most recent message content when the run is completed
+        thread_messages = client.beta.threads.messages.list(
+            thread_id = thread.id
+        )
+        message = thread_messages.data[0].content[0].text.value
 
-#     if run.status in ["cancelled", "failed", "expired"]:
-#         message = "An error has occurred, please try again."
+    if run.status in ["cancelled", "failed", "expired"]:
+        message = "An error has occurred, please try again."
 
-#     return message
+    return message
 
-# #check status details
-# def process_run(thread_id, assistant_id):
-#     #create a run
-#     new_run = client.beta.threads.runs.create(
-#         thread_id = thread_id,
-#         assistant_id = assistant_id
-#     )
+#check status details
+def process_run(thread_id, assistant_id):
+    #create a run
+    new_run = client.beta.threads.runs.create(
+        thread_id = thread_id,
+        assistant_id = assistant_id
+    )
 
-#     phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
+    phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
 
-#     #monitor the run status at regular intervals until the status of the run is complete
-#     while True:
-#         #add a one-second delary between each run status check
-#         time.sleep(1)
-#         print(random.choice(phrases) + "...")
-#         #check the status of the run
-#         run_check = client.beta.threads.runs.retrieve(
-#             thread_id = thread_id,
-#             run_id = new_run.id
-#         )
-#         if run_check.status in ["cancelled", "failed", "completed", "expired"]:
-#             return run_check
+    #monitor the run status at regular intervals until the status of the run is complete
+    while True:
+        #add a one-second delary between each run status check
+        time.sleep(1)
+        print(random.choice(phrases) + "...")
+        #check the status of the run
+        run_check = client.beta.threads.runs.retrieve(
+            thread_id = thread_id,
+            run_id = new_run.id
+        )
+        if run_check.status in ["cancelled", "failed", "completed", "expired"]:
+            return run_check
         
-# #log error if status is cancelled, failed, or expired
-# def log_run(run_status):
-#       if run_status in ["cancelled", "failed", "completed", "expired"]:
-#           log.error("\nDate: " + str(datetime.datetime.now()) + "\nRun status: " + str(run_status))
+#log error if status is cancelled, failed, or expired
+def log_run(run_status):
+    if run_status in ["cancelled", "failed", "completed", "expired"]:
+        log.error("\nDate: " + str(datetime.datetime.now()) + "\nRun status: " + str(run_status))
 
-# #create a thread
-# thread = client.beta.threads.create()
+#create a thread
+thread = client.beta.threads.create()
 
-# user_input = ""
+user_input = ""
 
-# while True:
-#     if (user_input == ""):
-#         user_input = input("Hello! Let's chat! You can type `exit` to exit out anytime. What's your name? ")
-#         #the model doesn't always recognize the answer as a name
-#         #hand it directly to the chatbot identified as the user's nameso
-#         user_name = f"User name is {user_input}"
-#     else:
-#         #prompt the user for input
-#         user_input = input("You: ")
+while True:
+    if (user_input == ""):
+        user_input = input("Hello! Let's chat! You can type `exit` to exit out anytime. What's your name? ")
+        #the model doesn't always recognize the answer as a name
+        #hand it directly to the chatbot identified as the user's nameso
+        user_name = f"User name is {user_input}"
+    else:
+        #prompt the user for input
+        user_input = input("You: ")
 
-#     if user_input.lower() == "exit":
-#         print("Bye bye now, have a good one!")
-#         exit()
+    if user_input.lower() == "exit":
+        print("Bye bye now, have a good one!")
+        exit()
 
-#     #use the prompt to create a message within the thread
-#     message = client.beta.threads.messages.create(
-#         thread_id = thread.id,
-#         role = "user",
-#         content = user_input
-#     )
+    #use the prompt to create a message within the thread
+    message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = user_input
+    )
 
-#     run = process_run(thread.id, assistant.id)
+    run = process_run(thread.id, assistant.id)
 
-#     log_run(run.status)
+    log_run(run.status)
 
-#     message = status_message(run.status)
+    message = status_message(run.status)
 
-#     print("\nAssistant: " + message + "\n")
+    print("\nAssistant: " + message + "\n")
